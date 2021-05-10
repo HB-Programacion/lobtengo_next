68f6e033e896aef05fae83000b7e978212a8a525
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-05-10 16:59:11 -0500

    iniciando proyecto

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const error404 = () => {
+  return (
+    <div>
+      <p>Error 404</p>
+    </div>
+  )
+}
+
+export default error404
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+
+const index = () => {
+  return (
+    <div>
+      <p>Bienvenido a LOBTENGO</p>
+    </div>
+  )
+}
+
+export default index
