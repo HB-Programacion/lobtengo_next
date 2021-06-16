67722fe7d9c14d58c979a5a8404c8bb0cff42836
Napster <rome.1205h@gmail.com>
2021-06-16 17:28:31 -0500

    banner/steps home

---
+++ components/BannerCounter.js
@@ -0,0 +1,94 @@
+import React, {Component, useState} from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+
+const BannerCounter = () => {
+
+
+    return (
+        <>
+            <Container fluid="true">
+                <Row className="justify-content-center">
+                    <div className="section-counter">
+                        <Col xs={12} sm={2} md={2} lg={2} xl={2} className="align-self-center">
+                            <h3 className="title-counter">¡Cada vez MÁS cerca!</h3>
+                        </Col>
+                        <Col xs={12} sm={3} md={3} lg={3} xl={3}  className="align-self-center">
+                            <div className="graphic-count">
+                                <div className="count-days">
+                                    <h3>5 días</h3>
+                                </div>
+                                <div className="count-time">
+                                    <Row>
+                                        <Col>18h</Col>
+                                        <Col>15m</Col>
+                                        <Col>6s</Col>
+                                    </Row>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={7} md={7} lg={7} xl={7}>
+                            <div className="card-texto">
+                                <Card.Body>
+                                    <div className="text-cabecera">Llévate un departamento</div>
+                                    <div className="text-title">¡Desde S/60 soles!</div>
+                                    <div className="text-subtitle">Al tener un número limitado de tickets a la venta <span>tus posibilidades de ganar nunca disminuirán</span>, pero sí podrás aumentarlas. ¡Tú decides cuánto! Regístrate AHORA
+                                    </div>
+                                </Card.Body>
+                            </div>
+                        </Col>
+                    </div>
+                </Row>
+            </Container>
+            <style jsx>
+                {`
+                    .section-counter{
+                        display: flex;
+                        padding: 3rem;
+                        width: 80%;
+                        background-color: #FC6625;
+                        margin: 8rem auto;
+                        border-radius: 1rem;
+                    }
+                    .title-counter{
+                        font-family: "Baloo-Regular";
+                        font-weight: 900;
+                        font-size: 40px;
+                        color: #ffffff;
+                    }
+                    .card-texto {
+                        text-align: center;
+                        background: #ffffff;
+                        border-radius: 15px;
+                        font-family: "mont-regular" !important;
+                        padding: 1.5rem;
+                    }
+                    .text-cabecera{
+                        color: #878787;
+                        font-family: "Baloo-Regular";
+                        font-size: 18px;
+                        text-align: start;
+                    }
+                    .text-title{
+                        color: #3A487F;
+                        font-family: "Baloo-Regular";
+                        font-size: 30px;
+                        font-weight: 900;
+                        text-align: start;
+                    }
+                    .text-subtitle{
+                        color: #878787;
+                        font-family: "neutra-text-bold";
+                        font-size: 15px;
+                        font-weight: 300;
+                        text-align: start;
+                    }
+                    .text-subtitle span{
+                        color: #FC6625;
+                    }
+                `}
+            </style>
+        </>
+    )
+}
+
+export default BannerCounter;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import AppLayout from "../components/AppLayout";
 import { CardDeck, Carousel, Col, Container, Row } from "react-bootstrap";
 import CardStep from '../components/CardStep';
+import BannerCounter from '../components/BannerCounter'
 
 const index = () => {
 
@@ -27,35 +28,84 @@ const index = () => {
   return (
     <AppLayout>
       <div className="box-home">
-        <Carousel className="box-carousel">
-          {banner.map((ban) => (
-            <Carousel.Item className="carousel-item">
-              <img src={ban.image} alt="" className="w-100" />
-            </Carousel.Item>
-          ))}
-        </Carousel>
-        <CardStep/>
+        <div className="section-banner">
+          <Carousel className="box-carousel">
+            {banner.map((ban) => (
+              <Carousel.Item className="carousel-item">
+                <img src={ban.image} alt="" className="w-100" />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </div>
+        <div className="section-step">
+          <Container fluid="true">
+              <Row className="step">
+                <Col xs={12} sm={3} md={3} lg={3} xl={3}>
+                  <div className="text-comolobtengo">
+                    <h3 className="title-lobtengo">¿Cómo Lobtengo?</h3>
+                    <h6 className="subtitle-first">
+                      Participar del sorteo es muy simple.
+                    </h6>
+                    <h6 className="subtitle-second">
+                      ¡Solo son 3 pasos y listo!
+                    </h6>
+                    <button className="btn-participa">
+                      PARTICIPA AHORA
+                    </button>
+                  </div>
+                </Col>
+                <Col xs={12} sm={9} md={9} lg={9} xl={9}>
+                  <CardStep/>
+                </Col>
+              </Row>
+          </Container>
+        </div>
+        <BannerCounter/>
       </div>
       <style jsx>
         {`
-          .box-banner {
-            height: 50rem;
-            width: auto;
-            position: relative;
-          }
-
-          .view-mobile {
-            display: none;
-          }
-
-          .box-home {
+        .section-banner {
             background-size: cover;
             justify-content: left;
             align-items: right;
             border-bottom: 1px solid #5a5a5a;
           }
-
-          @media (max-width: 768px) {
+          .text-comolobtengo {
+            text-align: center;
+            font-family: "mont-regular";
+            margin-top: 10rem;
+            padding: 10px 60px;
+            text-align: start;
+          }
+          .title-lobtengo{
+            font-family: "Baloo-Regular";
+            font-weight: 700;
+            font-size: 38px;
+            color: #3A487F;
+          }
+          .subtitle-first{
+            font-family: "neutra-text-bold";
+            color: #878787;
+            font-size: 20px;
+          }
+          .subtitle-second{
+            font-family: "neutra-text-bold";
+            color: #FC6625;
+            font-size: 15px;
+          }
+          .btn-participa{
+            background-color: #3A487F;
+            border: none;
+            padding: 7px 10px;
+            color: #ffffff;
+            border-radius: 20px;
+            margin-top: 10px;
+            font-family: "neutra-text-bold";
+          }
+          .view-mobile {
+            display: none;
+          }
+        @media (max-width: 768px) {
             .view-mobile {
               display: block;
             }
