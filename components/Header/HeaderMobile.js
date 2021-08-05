c51548e7b179461d3c6d2eb837ca71b1abf41401
NardaC <nardac6@gmail.com>
2021-08-04 19:34:58 -0500

    menu-mobile

--- components/Header/HeaderMobile.js
+++ components/Header/HeaderMobile.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 const HeaderMobile = (props) => {
+  const [expand, setExpand] = useState(false);
+
   let drawerClasses = "side__drawer";
   if (props.menuBurgerOpen) {
     drawerClasses = "side__drawer open";
@@ -10,7 +12,7 @@ const HeaderMobile = (props) => {
     <>
       <nav className={drawerClasses}>
         <p className="cerrar" onClick={props.ocultarMenuMobile}>
-          cerrar menú X
+          cerrar menú &#10006;
         </p>
 
         <ul className="header__mobile">
@@ -19,32 +21,100 @@ const HeaderMobile = (props) => {
               <Image
                 src="/images/header/user.png"
                 alt="user lobtengo"
-                width={50}
-                height={50}
+                width={55}
+                height={55}
               />
             </div>
           </div>
           <div className="header__mobile-item ">
             <div>
-              <li className="header__mobile-nombre">Sebastian de la Guarda</li>
+              <li className="header__mobile-nombre">Sebastian</li>
               <p className="header__mobile-correo">hola@gmail.com</p>
             </div>
           </div>
-          <li>
-            <img src="/images/header/perfil.svg" />
+          <li className="center-item">
+            <img src="/images/header/perfil.svg" className="mr-1 img-item" />
             Mi perfil
           </li>
-          <li>
-            <img src="/images/header/compras.svg" />
+          <li className="center-item">
+            <img src="/images/header/compras.svg" className="mr-1 img-item" />
             Mis compras
           </li>
-          <hr/>
-          <li>Inicio</li>
-          <li>Nosotros</li>
-          <li>Como Funciona</li>
-          <li>Participa</li>
-          <li>El depa</li>
-          <li>Ayuda</li>
+          <hr />
+          <div className="box-items">
+            <li>
+              <img
+                src="/images/header/dot-circle.svg"
+                className="mr-1 img-item"
+              />
+              Inicio
+            </li>
+            <li>
+              <img
+                src="/images/header/dot-circle.svg"
+                className="mr-1 img-item"
+              />
+              Nosotros
+            </li>
+            <li>
+              <img
+                src="/images/header/dot-circle.svg"
+                className="mr-1 img-item"
+              />
+              Como Funciona
+            </li>
+            <li>
+              <img
+                src="/images/header/dot-circle.svg"
+                className="mr-1 img-item"
+              />
+              Participa
+            </li>
+            <li>
+              <img
+                src="/images/header/dot-circle.svg"
+                className="mr-1 img-item"
+              />
+              El depa
+            </li>
+            <div className="box-ayuda">
+              <li>
+                <img
+                  src="/images/header/dot-circle.svg"
+                  className="mr-1 img-item"
+                />
+                Ayuda
+                <span onClick={() => setExpand((expand) => !expand)}>
+                  <img
+                    src="/images/header/arrow-down.svg"
+                    className="arrow-down"
+                  />
+                </span>
+              </li>
+              {expand && (
+                <div className="box-ayuda-items">
+                  <p>Preguntas frecuentes</p>
+                  <p>Cóntactanos</p>
+                  <p>Encuesta</p>
+                </div>
+              )}
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </div>
+          </div>
+          <hr />
+          <li>
+            <img
+              src="/images/header/cerrar-sesion.svg"
+              className="mr-1 img-item"
+            />
+            Cerrar sesión
+          </li>
         </ul>
       </nav>
       <style jsx>
@@ -60,11 +130,11 @@ const HeaderMobile = (props) => {
             position: fixed;
             top: 0;
             right: 0%;
-            width: 80%;
+            width: 70%;
             z-index: 9999;
             transition: transform 0.3s ease-out;
             transform: translateX(120%);
-            padding: 2rem 1.5rem;
+            padding: 1.5rem 2rem;
           }
           .side__drawer.open {
             transform: translateX(0%);
@@ -76,8 +146,21 @@ const HeaderMobile = (props) => {
             align-items: flex-start;
             flex-direction: column;
             color: #ffff;
-            font-size: 1.2rem;
+            font-size: 1rem;
             list-style: none;
+            font-family: "neutra-text-bold";
+          }
+          .header__mobile li {
+            display: flex;
+            align-items: center;
+          }
+          .center-item {
+            display: flex;
+            align-items: center;
+            padding: 0.2rem 0;
+          }
+          .box-items li {
+            padding: 0.5rem 0;
           }
           .cerrar {
             font-size: 1rem;
@@ -92,7 +175,7 @@ const HeaderMobile = (props) => {
           }
 
           .header__mobile-nombre {
-            font-family: "neutra-text-bold";
+            font-size: 1.3rem;
           }
           .header__mobile-correo {
             font-family: "neutra-text-ligth";
@@ -102,17 +185,49 @@ const HeaderMobile = (props) => {
 
           .box-user {
             background-color: #3a487f;
-            width: 5rem;
-            height: 5rem;
+            width: 5.2rem;
+            height: 5.2rem;
             border-radius: 50%;
             position: absolute;
-            top: 3.46rem;
-            left: -1.5rem;
+            top: 3rem;
+            left: -2.3rem;
           }
           .user-lobtengo {
             position: relative;
             left: 1rem;
             top: 0.9rem;
+          }
+
+          hr {
+            width: 150%;
+            margin-left: -3.5rem;
+            background-color: #ffff !important;
+            opacity: 1;
+            height: 0.2px !important;
+            color: #ffff;
+          }
+          .mr-1 {
+            margin-right: 0.5rem;
+          }
+          .img-item {
+            width: 1rem;
+          }
+          .box-ayuda {
+            position: relative;
+          }
+
+          .box-ayuda-items {
+            margin-left: 1.4rem;
+            font-family: "neutra-text-ligth";
+            font-size: 0.9rem;
+            transition: transform 0.3s ease-out;
+            position:fixed
+          }
+          .box-ayuda-items p {
+            margin-bottom: 0.2rem;
+          }
+          .arrow-down {
+            margin-left: 0.5rem;
           }
         `}
       </style>
